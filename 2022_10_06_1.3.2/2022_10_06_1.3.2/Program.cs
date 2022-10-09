188e315681a710800b6dc9d9c2f8e23b402fd190
michael-hiegl <100510019+michael-hiegl@users.noreply.github.com>
2022-10-09 17:18:36 +0200

    Add files via upload

---
+++ 2022_10_06_1.3.2/2022_10_06_1.3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace _2022_10_06_1._3._2
+{
+    //Definition der Klasse Auto
+    class Auto
+    {
+        //Instanzvariablen
+        public int Reifen;
+        public double Motor;
+        public string Türen;
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaration einer Objektvariable
+            Auto Model1;
+
+            //Instanzierung
+            Model1 = new Auto();//oder Auto Model1 = new Auto();
+        }
+    }
+}
